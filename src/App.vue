c24990d3ec9d721011ac56e27e57c7c37152e531
旗木卡卡西 <lx@163.com>
2018-03-24 17:20:08 +0800

    脚手架

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <m-head></m-head>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import mHead from '@/components/m-head/m-head'
+export default {
+  name: 'App',
+  components : {
+    mHead
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
---
+++ src/components/m-head/m-head.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-container>
+    <el-header>我是页面头部</el-header>
+  </el-container>
+</template>
+
+<script>
+    export default {
+        name: "m-head"
+    }
+</script>
+
+<style scoped>
+
+</style>
